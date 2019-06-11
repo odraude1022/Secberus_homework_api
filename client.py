6b6de154b36552eb70ada294df457444d255729c
Eduardo Iglesias <eriglesias@ufl.edu>
2019-06-11 15:23:45 -0400

    extract authentication into separate function

--- client.py
+++ client.py
@@ -1,12 +1,17 @@
 import aiohttp
 import asyncio
 
+login = {"username": "guest", "password": "guest"}
+
+async def authenticate(login, session):
+    async with session.post('http://localhost:5000/api/login', json=login) as resp:
+        print(resp.status)
+        return (await resp.json()).get("access_token")
+
+
 async def solution():
     async with aiohttp.ClientSession() as session:
-        login = {"username": "guest", "password": "guest"}
-        async with session.post('http://localhost:5000/api/login', json=login) as resp:
-            print(resp.status)
-            token = (await resp.json()).get("access_token")
+        token = await authenticate(login, session)
         headers={"Authorization": f"Bearer {token}"}
         print(headers)
         async with session.get('http://localhost:5000/api/secret1', headers=headers) as resp:
@@ -14,7 +19,6 @@ async def solution():
             with open('secret1.txt', 'w') as f:
                 answer1 = (await resp.json()).get("answer")
                 f.write(answer1)
-                f.close()
                 print(answer1)
         async with session.get('http://localhost:5000/api/secret2', headers=headers) as resp:
             print(resp.status)
@@ -22,7 +26,6 @@ async def solution():
                 with open('secret2.txt', 'w') as f:
                     answer2 = (await resp.json()).get("answer")
                     f.write(answer2)
-                    f.close()
                     print(answer2)
         async with session.get('http://localhost:5000/api/secret3', headers=headers) as resp:
             print(resp.status)
@@ -30,7 +33,6 @@ async def solution():
                 with open('secret3.txt', 'w') as f:
                     answer3 = (await resp.json()).get("answer")
                     f.write(answer3)
-                    f.close()
                     print(answer3)
         await session.close()
 

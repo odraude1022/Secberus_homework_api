d9c752874b3a539594b99fee4c69f13d16cc2575
Eduardo Iglesias <eriglesias@ufl.edu>
2019-06-11 15:36:14 -0400

    implemented re-authentication

--- client.py
+++ client.py
@@ -5,35 +5,48 @@ login = {"username": "guest", "password": "guest"}
 
 async def authenticate(login, session):
     async with session.post('http://localhost:5000/api/login', json=login) as resp:
-        print(resp.status)
-        return (await resp.json()).get("access_token")
+        token = (await resp.json()).get("access_token")
+        return {"Authorization": f"Bearer {token}"}
 
-
-async def solution():
-    async with aiohttp.ClientSession() as session:
-        token = await authenticate(login, session)
-        headers={"Authorization": f"Bearer {token}"}
-        print(headers)
-        async with session.get('http://localhost:5000/api/secret1', headers=headers) as resp:
-            print(resp.status)
+async def secret1(session, headers):
+    async with session.get('http://localhost:5000/api/secret1', headers=headers) as resp:
+        if(resp.status == 401):
+            headers = await authenticate(login, session)
+            await secret1(session, headers)
+        else:
             with open('secret1.txt', 'w') as f:
                 answer1 = (await resp.json()).get("answer")
                 f.write(answer1)
                 print(answer1)
-        async with session.get('http://localhost:5000/api/secret2', headers=headers) as resp:
-            print(resp.status)
-            if(resp.status == 200):
-                with open('secret2.txt', 'w') as f:
-                    answer2 = (await resp.json()).get("answer")
-                    f.write(answer2)
-                    print(answer2)
-        async with session.get('http://localhost:5000/api/secret3', headers=headers) as resp:
-            print(resp.status)
-            if(resp.status == 200):
-                with open('secret3.txt', 'w') as f:
-                    answer3 = (await resp.json()).get("answer")
-                    f.write(answer3)
-                    print(answer3)
-        await session.close()
+
+async def secret2(session, headers):
+    async with session.get('http://localhost:5000/api/secret2', headers=headers) as resp:
+        if(resp.status == 401):
+            headers = await authenticate(login, session)
+            await secret2(session, headers)
+        else:
+            with open('secret2.txt', 'w') as f:
+                answer2 = (await resp.json()).get("answer")
+                f.write(answer2)
+                print(answer2)
+
+async def secret3(session, headers):
+    async with session.get('http://localhost:5000/api/secret3', headers=headers) as resp:
+        if(resp.status == 401):
+            headers = await authenticate(login, session)
+            await secret3(session, headers)
+        else:
+            with open('secret3.txt', 'w') as f:
+                answer3 = (await resp.json()).get("answer")
+                f.write(answer3)
+                print(answer3)
+
+
+async def solution():
+    async with aiohttp.ClientSession() as session:
+        headers = await authenticate(login, session)
+        await secret1(session, headers)
+        await secret2(session, headers)
+        await secret3(session, headers)
 
 asyncio.run(solution())

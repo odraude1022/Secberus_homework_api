f37d4db83a1edf39bde2fcfe4702a1c8053c63b1
Eduardo Iglesias <eriglesias@ufl.edu>
2019-06-11 17:04:50 -0400

    refactor using loops

--- client.py
+++ client.py
@@ -1,29 +1,29 @@
-import aiohttp
 import asyncio
+import aiohttp
+from pathlib import Path
 
 login = {"username": "guest", "password": "guest"}
+url = 'http://localhost:5000/api/{}'
 
 async def authenticate(login, session):
-    async with session.post('http://localhost:5000/api/login', json=login) as resp:
+    async with session.post(url.format('login'), json=login) as resp:
         token = (await resp.json()).get("access_token")
         return {"Authorization": f"Bearer {token}"}
 
 async def secret(session, headers, num):
-    async with session.get(f'http://localhost:5000/api/secret{num}', headers=headers) as resp:
+    async with session.get(url.format(f'secret{num}'), headers=headers) as resp:
         if(resp.status == 401):
             headers = await authenticate(login, session)
             return(await secret(session, headers, num))
         else:
-            with open(f'secret{num}.txt', 'w') as f:
-                answer = (await resp.json()).get("answer")
-                f.write(answer)
-                return answer
+            answer = (await resp.json()).get("answer")
+            Path(f'secret{num}.txt').write_text(answer)
+            return answer
 
 async def solution():
     async with aiohttp.ClientSession() as session:
         headers = await authenticate(login, session)
-        print(await secret(session, headers, 1))
-        print(await secret(session, headers, 2))
-        print(await secret(session, headers, 3))
+        for i in range(1, 4):
+            print(await secret(session, headers, i))
 
 asyncio.run(solution())
